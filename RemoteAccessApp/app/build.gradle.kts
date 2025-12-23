33e85fe45128b63331d02743f370b43747041d77
bhikandeshmukh <thebhikandeshmukh@gmail.com>
2025-12-23 19:17:33 +0530

    Fix Gradle and Kotlin versions for CI build

--- RemoteAccessApp/app/build.gradle.kts
+++ RemoteAccessApp/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.ksp)
     alias(libs.plugins.google.services)
-    kotlin("plugin.serialization") version "1.9.21"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 android {
